b847d1ad1116638096fe1cc5eab4ae013b6bedff
VivaanMahapatra <vivaanmahapatra51@gmail.com>
2025-10-19 15:00:41 +0530

    Add files via upload

---
+++ ACP Birthday dates.py
@@ -0,0 +1,7 @@
+print("\nUse format dd/mm/yyyy while entering the birthday dates\n")
+bday_date1 = input("Please enter the first birthday date : ")
+bday_date2 = input("Please enter the second birthday date : ")
+bday_date3 = input("Please enter the third birthday date : ")
+bday_date4 = input("Please enter the fourth birthday date : ")
+bday_date5 = input("Please enter the fifth birthday date : ")
+print("\nthe 5 birthday dates are : ", bday_date1, bday_date2, bday_date3, bday_date4, bday_date5)25
